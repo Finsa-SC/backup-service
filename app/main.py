c218bd0fb1b0bc4d1fc18ca3d7d82d537ba8757b
Finsa-SC <finsakusumaputra@gmail.com>
2026-07-29 15:06:41 +0700

    refactor(main): refactor from using self.args to return parse from get_config

--- app/main.py
+++ app/main.py
@@ -14,7 +14,6 @@ logger = logging.getLogger(__name__)
 
 class BackupService:
     def __init__(self):
-        self.args = None
         self.config = None
 
     def __enter__(self):
@@ -25,30 +24,32 @@ class BackupService:
             logger.info(exc_val)
         return False
 
-    def get_argument(self):
+    @staticmethod
+    def get_config():
         parser = argparse.ArgumentParser(description="Backup Service")
         parser.add_argument(
             "--config",
             required=True,
             help="Path to Configuration target",
         )
-        self.args = parser.parse_args()
+        return parser.parse_args()
 
     def load_config(self):
-        config_path = Path(self.args.config)
+        args = self.get_config()
+        config_path = Path(args.config)
         try:
             with open(config_path, "rb") as file:
                 self.config = tomllib.load(file)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"File {self.args.config} not found!")
+        except FileNotFoundError as er:
+            raise FileNotFoundError(f"Configuration file not found: {args.config}") from er
 
     def run(self):
-        self.get_argument()
+        self.get_config()
         self.load_config()
-        BACKUP = self.config.get("backup")
-        target_backup = BACKUP.get("target", None)
-        destination = BACKUP.get("destination", None)
-        backup_name = BACKUP.get("backup_name", None)
+        backup = self.config.get("backup")
+        target_backup = backup.get("target", None)
+        destination = backup.get("destination", None)
+        backup_name = backup.get("backup_name", None)
 
         backuper = Backuper(target_path=target_backup, destination_path=destination, backup_name=backup_name)
         backuper.do_backup()
@@ -56,8 +57,8 @@ class BackupService:
 if __name__ == "__main__":
     try:
         logger.info("Starting backup service...")
-        backup = BackupService()
-        backup.run()
+        backup_service = BackupService()
+        backup_service.run()
         logger.info("Target has been backup!")
     except Exception as e:
         logger.error(e)
