318a7d5e56470c9ce392295d0a105a4569adff61
Finsa-SC <finsakusumaputra@gmail.com>
2026-07-29 14:52:11 +0700

    refactor(main): refactor from using hardcode config to argsparse config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-import tomllib, logging, sys
+import tomllib, logging, sys, argparse
 from pathlib import Path
 from backuper import Backuper
 
+# Set logging
 logging.basicConfig(
     format="[%(levelname)s] %(asctime)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
@@ -10,38 +11,53 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-current_directory = Path(__file__).resolve().parents[1]
-with open(current_directory / "config.toml", "rb") as file:
-    config = tomllib.load(file)
 
 class BackupService:
     def __init__(self):
-        ...
+        self.args = None
+        self.config = None
 
     def __enter__(self):
         return self
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type:
             logger.info(exc_val)
-        return True
+        return False
 
-    def backup_target(self, item):
-        target_backup = item.get("target", None)
-        destination = item.get("destination", None)
-        backup_name = item.get("backup_name", None)
+    def get_argument(self):
+        parser = argparse.ArgumentParser(description="Backup Service")
+        parser.add_argument(
+            "--config",
+            required=True,
+            help="Path to Configuration target",
+        )
+        self.args = parser.parse_args()
+
+    def load_config(self):
+        self.get_argument()
+        config_path = Path(self.args.config)
+        try:
+            with open(config_path, "rb") as file:
+                self.config = tomllib.load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {self.args.config} not found!")
+
+    def run(self):
+        self.load_config()
+        BACKUP = self.config.get("backup")
+        target_backup = BACKUP.get("target", None)
+        destination = BACKUP.get("destination", None)
+        backup_name = BACKUP.get("backup_name", None)
 
         backuper = Backuper(target_path=target_backup, destination_path=destination, backup_name=backup_name)
         backuper.do_backup()
-
-    def run(self):
-        for item in config["backup"]:
-            self.backup_target(item)
 
 if __name__ == "__main__":
     try:
         logger.info("Starting backup service...")
         backup = BackupService()
         backup.run()
-        logger.info("All target has been backup!")
+        logger.info("Target has been backup!")
     except Exception as e:
         logger.error(e)
