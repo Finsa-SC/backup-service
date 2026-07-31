04516054f253b4852c3dce3806dd6929aba7eb72
Finsa-SC <finsakusumaputra@gmail.com>
2026-07-31 10:26:15 +0700

    style(archive): rename some variable

--- backuper_app/backup/archive.py
+++ backuper_app/backup/archive.py
@@ -4,10 +4,10 @@ from backuper_app.utils import get_logger
 logger = get_logger(__name__)
 
 class Archive:
-    def __init__(self, oldest_backup: list[Path], archive_path: Path = None, archive_enable: bool = False):
+    def __init__(self, expired_backups: list[Path], archive_path: Path = None, archive_enabled: bool = False):
         self._archive_path = archive_path
-        self._oldest_backup = oldest_backup
-        self.archive_enable = archive_enable
+        self._expired_backups = expired_backups
+        self.archive_enabled = archive_enabled
 
     def _move_into_archive(self, old_backup: Path):
         old_backup.move_into(self._archive_path)
@@ -17,13 +17,13 @@ class Archive:
         old_backup.unlink()
 
     def do_archive(self):
-        for old in self._oldest_backup:
-            if self.archive_enable:
+        for old in self._expired_backups:
+            if self.archive_enabled:
                 logger.debug(f"Moving {old.name} into {self._archive_path}...")
                 self._move_into_archive(old)
                 logger.info(f"{old.name} has been moved to {self._archive_path.name}")
             else:
                 logger.debug(f"Deleting {old.name} from {self._archive_path}")
                 self._delete_from_backup(old)
-                logger.info(f"{old.name} has beed removed from {self._archive_path.name}")
+                logger.info(f"{old.name} has beed removed")
 
