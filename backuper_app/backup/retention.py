aa036cbde465eed35cbfa142eeb3911f639276b2
Finsa-SC <finsakusumaputra@gmail.com>
2026-08-02 13:01:35 +0700

    refactor(retention): refactor retention tu manage checksum backup file and store into expired backup

--- backuper_app/backup/retention.py
+++ backuper_app/backup/retention.py
@@ -10,9 +10,11 @@ class Retention:
         self._keep_last = keep_last
 
     def get_backup_glob(self) -> list[Path]:
-        backups_list = list(
+        path_list = list(
             self._destination.glob(f"{self._backup_name}*")
         )
+        backups_list = [backup for backup in path_list if not str(backup).endswith(".sha256")]
+
         backups_list.sort(key=lambda backup: backup.name)
         return backups_list
 
@@ -20,15 +22,20 @@ class Retention:
         return len(backups) > self._keep_last
 
     def get_should_delete(self, oldest_backups: list[Path]):
-        violating_policy = []
+        expired_backups = []
         while self.should_delete_backup(oldest_backups):
-            violating_policy.append(oldest_backups.pop(0))
+            oldest_backup = oldest_backups.pop(0)
+            expired_backups.append(oldest_backup)
 
-        return violating_policy
+            backup_checksum = oldest_backup.with_name(oldest_backup.name + ".sha256")
+            if backup_checksum.is_file():
+                expired_backups.append(backup_checksum)
+
+        return expired_backups
 
     def do_retention(self):
-        backups = self.get_backup_glob()
-        should_delete = self.get_should_delete(backups)
+        expired_backups = self.get_backup_glob()
+        should_delete = self.get_should_delete(expired_backups)
 
         return should_delete
 
