05e490dcdf4a69da3554e856532423b223b84f8e
Finsa-SC <finsakusumaputra@gmail.com>
2026-08-02 13:08:00 +0700

    feat(checksum): add function to verify checksum and move context manager file make_hash -> get_hash

--- backuper_app/utils/checksum.py
+++ backuper_app/utils/checksum.py
@@ -11,25 +11,30 @@ def make_file_checksum(path_file: Path, hashed_file: str) -> Path:
 
     return checksum_path
 
-def get_hash(file) -> str:
-    sha = hashlib.sha256()
-    while True:
-        byte = file.read(8096)
+def get_hash(file_path) -> str:
+    with file_path.open('rb') as file:
+        sha = hashlib.sha256()
+        while True:
+            byte = file.read(8096)
 
-        if byte:
-            sha.update(byte)
-        else:
-            break
-    return sha.hexdigest()
+            if byte:
+                sha.update(byte)
+            else:
+                break
+        return sha.hexdigest()
 
 def make_hash(file_path: Path):
     if file_path.is_file():
-        with file_path.open('rb') as file:
-            hashed_file = get_hash(file)
+            hashed_file = get_hash(file_path)
 
             return make_file_checksum(file_path, hashed_file)
     else:
-        raise FileNotFoundError(f"{file_path} not exists or it's not a file")
+        raise FileNotFoundError(f"{file_path} not exist or it's not a file")
+
+def is_valid_checksum(file_path: Path, checksum_path: Path):
+    hashed_file = get_hash(file_path)
+    hashed_checksum = get_hash(checksum_path)
+    return hashed_file == hashed_checksum
 
 if __name__ == "__main__":
     make_hash(Path("/home/silence-suzuka/backup_test/playground_20260731_182432.tar.zst"))
