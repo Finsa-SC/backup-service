3dbe0a23e5e18c7b3b70b45bbfe28605e085f548
Finsa-SC <finsakusumaputra@gmail.com>
2026-08-11 21:26:13 +0700

    feat(initializer): add initializer.py to create config service

---
+++ backuper_app/backup/initializer.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+class Initializer:
+    def __init__(
+            self,
+            config_path: Path,
+            target: Path,
+            destination: Path,
+            retention: int|None,
+            compression: str,
+            link_mode: str,
+    ):
+        self.config_path = config_path
+        self.target = target
+        self.destination = destination
+        self.retention = retention
+        self.compression = compression
+        self.link_mode = link_mode
+
+    def set_init(self):
+        return f"""
+[backup]
+target = {self.target}
+destination = {self.destination}
+backup_name = 
+compression = {self.compression}
+
+link_mode = {self.link_mode} #follow/preserve/ignore
+
+[filter]
+include = []
+exclude = [
+    ".venv",
+    "dist/",
+    "**.__pycache__/",
+    "**/*.pyc",
+]
+
+[retention]
+keep_last = 
+
+[archive]
+enabled = false
+path =
+"""
+
+    def make_init(self):
+        with self.config_path.open('w+') as file:
+            file.write(self.set_init())
